4101b6bfbc2b60c1175186138dc92f22a4860f61
mateus cristian <mateus_cristian@outlook.com>
2026-02-07 11:30:08 -0300

    makes `npm test` more robvust with `async-retry` and `orchestrator.js`

--- jest.config.js
+++ jest.config.js
@@ -15,6 +15,8 @@ const createJestConfig = nextJest({
 
 const jestConfig = createJestConfig({
   moduleDirectories: ["node_modules", "<rootDir>"],
+  setupFilesAfterEnv: ["<rootDir>/jest.setup.js"],
+  testTimeout: 60000,
 });
 
 module.exports = jestConfig;
