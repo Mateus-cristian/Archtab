2fffa533a855288173b017ac5c09b78ea8eb62ed
Mateus Cristian <mateus_cristian@outlook.com>
2026-03-04 10:51:57 -0300

    test: create tests `PUT: migrations` and `POST: status`

---
+++ tests/integration/api/v1/status/post.test.js
@@ -0,0 +1,28 @@
+import orchestrator from "tests/orchestrator";
+
+beforeAll(async () => {
+  await orchestrator.waitForAllServices();
+  await orchestrator.clearDatabase();
+});
+
+describe("POST /api/v1/status", () => {
+  describe("Anonymous user", () => {
+    test("Retriving current system status", async () => {
+      const response = await fetch("http://localhost:3000/api/v1/status", {
+        method: "POST",
+      });
+
+      const responseBody = await response.json();
+
+      expect(response.status).toBe(405);
+
+      expect(responseBody).toEqual({
+        name: "MethodNotAllowedError",
+        message: "Método não permitido para o endpoint.",
+        action:
+          "Verifique se o método HTTP enviado é válido para esse endpoint.",
+        status_code: 405,
+      });
+    });
+  });
+});
