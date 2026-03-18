9598ca4e5f518c3c385c1814ca673b0b4da0aaf8
Mateus Cristian <mateus_cristian@outlook.com>
2026-03-18 09:03:36 -0300

    feat: create `password` model and refator `create user` method

--- models/user.js
+++ models/user.js
@@ -1,9 +1,11 @@
 import database from "infra/database";
 import { NotFoundError, ValidationError } from "infra/errors";
+import password from "models/password.js";
 
 async function create(userInputValues) {
   await validateUniqueEmail(userInputValues.email);
   await validateUniqueUsername(userInputValues.username);
+  await hashPasswordInObject(userInputValues);
 
   const newUser = await runInsertQuery(userInputValues);
 
@@ -49,6 +51,12 @@ async function create(userInputValues) {
         action: "Utilize outro username para realizar o cadastro.",
       });
     }
+  }
+
+  async function hashPasswordInObject(userInputValues) {
+    const hashedPassword = await password.hash(userInputValues.password);
+
+    userInputValues.password = hashedPassword;
   }
 
   async function runInsertQuery(userInputValues) {
