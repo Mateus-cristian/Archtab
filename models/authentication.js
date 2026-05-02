aef4e69ce663607c67129fd725829249246a89a9
Mateus Cristian <mateus_cristian@outlook.com>
2026-05-02 10:33:42 -0300

    feat: implement session creation

---
+++ models/authentication.js
@@ -0,0 +1,58 @@
+import { NotFoundError, UnauthorizedError } from "infra/errors";
+import password from "models/password";
+import user from "models/user.js";
+
+async function getAuthenticateUser(providedEmail, providedPassword) {
+  try {
+    const storedUser = await findUserByEmail(providedEmail);
+    await validatePassword(providedPassword, storedUser.password);
+
+    return storedUser;
+  } catch (error) {
+    if (error instanceof UnauthorizedError) {
+      throw new UnauthorizedError({
+        message: "Dados de autenticação não conferem.",
+        action: "Verifique se os dados estão corretos.",
+      });
+    }
+
+    throw error;
+  }
+
+  async function findUserByEmail(providedEmail) {
+    try {
+      const storedUser = await user.findOneByEmail(providedEmail);
+
+      return storedUser;
+    } catch (error) {
+      if (error instanceof NotFoundError) {
+        throw new UnauthorizedError({
+          message: "Email não confere.",
+          action: "Verifique se este dado está correto.",
+        });
+      }
+
+      throw error;
+    }
+  }
+
+  async function validatePassword(providedPassword, storedPassword) {
+    const correctPasswordMatch = await password.compare(
+      providedPassword,
+      storedPassword,
+    );
+
+    if (!correctPasswordMatch) {
+      throw new UnauthorizedError({
+        message: "Senha não confere.",
+        action: "Verifique se este dado está correto.",
+      });
+    }
+  }
+}
+
+const authentication = {
+  getAuthenticateUser,
+};
+
+export default authentication;
