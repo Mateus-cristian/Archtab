6ef1f1031d40c2872b133cc743b65df2cdab3915
Mateus Cristian <mateus.cristian@pluga.co>
2026-05-26 10:04:40 -0300

    feat: add  column to mateuscristian mateuscristian table

---
+++ infra/migrations/1779798460376_add-features-to-users.js
@@ -0,0 +1,12 @@
+exports.up = (pgm) => {
+  pgm.addColumn("users", {
+    features:{
+      type: "varchar[]",
+      notNull: true,
+      default: "{}"
+    }
+  })
+};
+
+
+exports.down = (pgm) => false;
