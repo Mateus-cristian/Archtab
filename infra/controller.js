650c1ea25e7ff30c32da9eafd4c17872eb47e28c
Mateus Cristian <mateus.cristian@pluga.co>
2026-06-12 09:28:23 -0300

    feat: add `injectAnonymousOrUser` and `canRequest` middlewares to `sessions`

--- infra/controller.js
+++ infra/controller.js
@@ -1,5 +1,6 @@
 import * as cookie from "cookie";
 import session from "models/session";
+import user from "models/user";
 
 const {
   MethodNotAllowedError,
@@ -7,6 +8,7 @@ const {
   ValidationError,
   NotFoundError,
   UnauthorizedError,
+  ForbbidenError,
 } = require("./errors");
 
 function onNoMatchHandler(request, response) {
@@ -18,8 +20,13 @@ function onErrorHandler(error, request, response) {
   if (
     error instanceof ValidationError ||
     error instanceof NotFoundError ||
-    error instanceof UnauthorizedError
+    error instanceof ForbbidenError
   ) {
+    return response.status(error.statusCode).json(error);
+  }
+
+  if (error instanceof UnauthorizedError) {
+    clearSessionCookie(response);
     return response.status(error.statusCode).json(error);
   }
 
@@ -52,6 +59,54 @@ function clearSessionCookie(response) {
   response.setHeader("Set-Cookie", setCookie);
 }
 
+async function injectAnonymousOrUser(request, response, next) {
+  if (request.cookies?.session_id) {
+    await injectAuthenticateUser(request);
+
+    return next();
+  }
+
+  injectAnonymousUser(request);
+  return next();
+
+  async function injectAuthenticateUser(request) {
+    const sessionToken = request.cookies.session_id;
+    const sessionObject = await session.findOneValidByToken(sessionToken);
+    const userObject = await user.findOneById(sessionObject.user_id);
+
+    request.context = {
+      ...request.context,
+      user: userObject,
+    };
+  }
+
+  function injectAnonymousUser(request) {
+    const anonymousUserObject = {
+      features: ["read:activation_token", "create:session", "create:user"],
+    };
+
+    request.context = {
+      ...request.context,
+      user: anonymousUserObject,
+    };
+  }
+}
+
+function canRequest(feature) {
+  return function canRequestMiddleware(request, response, next) {
+    const userTryingToRequest = request.context.user;
+
+    if (userTryingToRequest.features.includes(feature)) {
+      return next();
+    }
+
+    throw new ForbbidenError({
+      message: "Você não possui permissão para executar esta ação.",
+      action: `Verifique se o seu usuário possui a feature ${feature}.`,
+    });
+  };
+}
+
 const controller = {
   errorHandlers: {
     onNoMatch: onNoMatchHandler,
@@ -59,6 +114,8 @@ const controller = {
   },
   setSessionCookie,
   clearSessionCookie,
+  injectAnonymousOrUser,
+  canRequest,
 };
 
 export default controller;
--- tests/integration/_use-cases/registration-flow.test.js
+++ tests/integration/_use-cases/registration-flow.test.js
@@ -82,7 +82,27 @@ describe("Use case: Registration Flow (all successful)", () => {
     expect(activedUser.features).toEqual(["create:session"]);
   });
 
-  test.todo("Login");
+  test("Login", async () => {
+    const createSessionResponse = await fetch(
+      "http://localhost:3000/api/v1/sessions",
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          email: "mateus.cris@curso.dev",
+          password: "senha123",
+        }),
+      },
+    );
+
+    expect(createSessionResponse.status).toBe(201);
+
+    const createSessionResponseBody = await createSessionResponse.json();
+
+    expect(createSessionResponseBody.user_id).toBe(createUserResponseBody.id);
+  });
 
   test.todo("Get user information");
 });
