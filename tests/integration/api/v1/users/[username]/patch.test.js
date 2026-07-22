0f93e3349d9ed3fe4d65cc34ff0fc548417abcb6
Mateus Cristian <mateus.cristian@pluga.co>
2026-07-22 09:46:07 -0300

    feat: add email confirm in test PATCH `api/v1/users/[username]`

--- tests/integration/api/v1/users/[username]/patch.test.js
+++ tests/integration/api/v1/users/[username]/patch.test.js
@@ -258,6 +258,10 @@ describe("PATCH /api/v1/users/[username]", () => {
       expect(Date.parse(responseBody.created_at)).not.toBeNaN();
       expect(Date.parse(responseBody.updated_at)).not.toBeNaN();
       expect(responseBody.updated_at > responseBody.created_at).toBe(true);
+
+      const userInDatabase = await user.findOneByUsername(activatedUser.username);
+
+      expect(userInDatabase.email).toBe("uniqueEmail2.novo@curso.dev");
     });
 
     test("With new `password`", async () => {
