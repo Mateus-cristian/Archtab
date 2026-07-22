f54c4249f4ae44ca62da948af2ec2611b217f984
Mateus Cristian <mateus.cristian@pluga.co>
2026-07-22 09:58:20 -0300

    refactor: chain routes on endpoint

--- tests/integration/api/v1/users/[username]/patch.test.js
+++ tests/integration/api/v1/users/[username]/patch.test.js
@@ -259,7 +259,9 @@ describe("PATCH /api/v1/users/[username]", () => {
       expect(Date.parse(responseBody.updated_at)).not.toBeNaN();
       expect(responseBody.updated_at > responseBody.created_at).toBe(true);
 
-      const userInDatabase = await user.findOneByUsername(activatedUser.username);
+      const userInDatabase = await user.findOneByUsername(
+        activatedUser.username,
+      );
 
       expect(userInDatabase.email).toBe("uniqueEmail2.novo@curso.dev");
     });
